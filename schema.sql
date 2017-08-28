c7cf5e057305c7a36133b87d6588e2635bacb1f9
Seth Etter <sethetter@gmail.com>
2017-08-27 22:40:42 -0500

    Basic schema and seeding

--- schema.sql
+++ schema.sql
@@ -4,7 +4,8 @@ create table actions (
        id            serial,
        name          varchar(64) not null,
        value         integer not null default 1,
-       week_days     varchar(7) not null default '1111100'
+       per_week      integer not null default 5,
+       per_day       integer not null default 1
 );
 
 create table action_logs (
@@ -12,9 +13,7 @@ create table action_logs (
        name          varchar(64) not null,
        value         integer not null default 1,
        action_id     integer,
-       confirmed_at  timestamp,
-       created_at    timestamp not null default current_timestamp,
-       updated_at    timestamp not null default current_timestamp
+       created_at    timestamp not null default current_timestamp
 );
 
 commit;
---
+++ seed.sql
@@ -0,0 +1,19 @@
+insert into actions (name, value, per_week, per_day)
+  values ('Brush teeth', 1, 5, 2);
+
+insert into actions (name, value, per_week, per_day)
+  values ('Clean room', 3, 1, 1);
+
+insert into actions (name, value, per_week, per_day)
+  values ('Homework', 4, 1, 1);
+
+-- Seed fake logs
+
+insert into action_logs (name, value, action_id, created_at)
+  values ('Brush teeth', 1, 1, current_timestamp);
+
+insert into action_logs (name, value, action_id, created_at)
+  values ('Brush teeth', 1, 1, current_timestamp);
+
+insert into action_logs (name, value, action_id, created_at)
+  values ('Clean room', 3, 2, date '08/28/17' + interval '1 hour');
