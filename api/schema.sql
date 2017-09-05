f6145e8dff4d46c79fdff07cdad1489b19824e67
Seth Etter <sethetter@gmail.com>
2017-09-04 19:59:04 -0500

    Color completed actions/logs, and create log

--- api/schema.sql
+++ api/schema.sql
@@ -12,8 +12,21 @@ create table action_logs (
        id            serial,
        name          varchar(64) not null,
        value         integer not null default 1,
-       action_id     integer,
+       action_id     integer not null,
        created_at    timestamp not null default current_timestamp
 );
+
+create or replace function create_action_log (action_id integer)
+returns void as $$
+begin
+       insert into action_logs (action_id, name, value)
+       values(
+        action_id,
+        (select name from actions where id = action_id limit 1),
+        (select value from actions where id = action_id limit 1)
+       );
+       select
+end
+$$ language 'plpgsql';
 
 commit;
--- src/Routinely.elm
+++ src/Routinely.elm
@@ -3,9 +3,11 @@ module Routinely exposing (..)
 import Date exposing (Date, Day)
 import Date.Extra.Format
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
 import Http
 import Json.Decode as Decode
+import Json.Encode as Encode
 import List
 import Time exposing (Time)
 import Task
@@ -20,11 +22,13 @@ main =
         , view = view
         }
 
+
 type alias Model =
     { actions : List Action
     , actionLogs : List ActionLog
     , theTime : Time
     }
+
 
 type alias Action =
     { id : Int
@@ -34,6 +38,7 @@ type alias Action =
     , perDay : Int
     }
 
+
 type alias ActionLog =
     { id : Int
     , name : String
@@ -41,6 +46,7 @@ type alias ActionLog =
     , actionId : Int
     , createdAt : String
     }
+
 
 init : ( Model, Cmd Msg )
 init =
@@ -51,35 +57,56 @@ init =
     , Task.perform CurrentTime Time.now
     )
 
+
 type Msg
     = NoOp
     | CurrentTime Time
-    | ActionsLoaded (Result Http.Error (List Action))
-    | ActionLogsLoaded (Result Http.Error (List ActionLog))
+    | GetActionsResponse (Result Http.Error (List Action))
+    | GetActionLogsResponse (Result Http.Error (List ActionLog))
+    | CreateActionLog Action
+    | CreateActionLogResponse (Result Http.Error String)
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
             model ! [ Cmd.none ]
+
         CurrentTime time ->
             model ! [ getActions, getActionLogs time ]
-        ActionsLoaded (Ok newActions) ->
+
+        GetActionsResponse (Ok newActions) ->
             { model | actions = newActions } ! [ Cmd.none ]
-        ActionLogsLoaded (Ok newActionLogs) ->
+
+        GetActionLogsResponse (Ok newActionLogs) ->
             { model | actionLogs = newActionLogs } ! [ Cmd.none ]
+
         -- TODO: Maybe retry on err?
-        ActionsLoaded (Err _) ->
+        GetActionsResponse (Err _) ->
             model ! [ Cmd.none ]
-        ActionLogsLoaded (Err _) ->
+
+        GetActionLogsResponse (Err _) ->
             model ! [ Cmd.none ]
+
+        CreateActionLog action ->
+            model ! [ postActionLog action ]
+
+        CreateActionLogResponse (Ok _) ->
+            model ! [ Task.perform CurrentTime Time.now ]
+
+        CreateActionLogResponse (Err _) ->
+            model ! [ Cmd.none ]
+
 
 getActions : Cmd Msg
-getActions = Http.send ActionsLoaded actionsRequest
+getActions =
+    Http.send GetActionsResponse getActionsRequest
 
 
-actionsRequest : Http.Request (List Action)
-actionsRequest = Http.get "http://localhost:3333/actions" actionDecoder
+getActionsRequest : Http.Request (List Action)
+getActionsRequest =
+    Http.get "http://localhost:3333/actions" actionDecoder
 
 
 actionDecoder : Decode.Decoder (List Action)
@@ -94,27 +121,68 @@ actionDecoder =
 
 
 getActionLogs : Time -> Cmd Msg
-getActionLogs time = Http.send ActionLogsLoaded <| actionLogsRequest time
+getActionLogs time =
+    Http.send GetActionLogsResponse <| actionLogsRequest time
 
 
 actionLogsRequest : Time -> Http.Request (List ActionLog)
 actionLogsRequest time =
-    let mostRecentMonday = time - ( ( daysAwayFromMonday time ) * 24.0 * Time.hour )
-        timestamp = Date.Extra.Format.isoDateString <| Date.fromTime mostRecentMonday
-        actionLogsUrl = "http://localhost:3333/action_logs?created_at=gt." ++ timestamp
-     in Http.get actionLogsUrl actionLogDecoder
+    let
+        mostRecentMonday =
+            time - ((daysAwayFromMonday time) * 24.0 * Time.hour)
+
+        timestamp =
+            Date.Extra.Format.isoDateString <| Date.fromTime mostRecentMonday
+
+        actionLogsUrl =
+            "http://localhost:3333/action_logs?created_at=gt." ++ timestamp
+    in
+        Http.get actionLogsUrl actionLogDecoder
+
+
+postActionLog : Action -> Cmd Msg
+postActionLog action =
+    Http.send CreateActionLogResponse <| postActionLogRequest action
+
+
+postActionLogRequest : Action -> Http.Request String
+postActionLogRequest action =
+    let
+        postActionLogUrl =
+            "http://localhost:3333/rpc/create_action_log"
+
+        json =
+            Encode.object <| [ ( "action_id", Encode.int action.id ) ]
+
+        body =
+            Http.jsonBody json
+    in
+        Http.post postActionLogUrl body (Decode.string)
 
 
 daysAwayFromMonday : Time -> Float
 daysAwayFromMonday time =
     case (Date.dayOfWeek <| Date.fromTime time) of
-        Date.Mon -> 0.0
-        Date.Tue -> 1.0
-        Date.Wed -> 2.0
-        Date.Thu -> 3.0
-        Date.Fri -> 4.0
-        Date.Sat -> 5.0
-        Date.Sun -> 6.0
+        Date.Mon ->
+            0.0
+
+        Date.Tue ->
+            1.0
+
+        Date.Wed ->
+            2.0
+
+        Date.Thu ->
+            3.0
+
+        Date.Fri ->
+            4.0
+
+        Date.Sat ->
+            5.0
+
+        Date.Sun ->
+            6.0
 
 
 actionLogDecoder : Decode.Decoder (List ActionLog)
@@ -134,55 +202,91 @@ view model =
         [ class "container"
         ]
         [ h1 []
-              [ text "Routinely!" ]
+            [ text "Routinely!" ]
         , table
             [ class "table table-bordered" ]
             [ thead []
                 [ viewTableHeaders ]
             , tbody []
-                ( viewWeeklyActions model.actions model.actionLogs )
+                (viewWeeklyActions model.actions model.actionLogs)
             ]
         ]
 
+
 weekDays : List Day
-weekDays = [ Date.Mon, Date.Tue, Date.Wed, Date.Thu, Date.Fri, Date.Sat, Date.Sun ]
+weekDays =
+    [ Date.Mon, Date.Tue, Date.Wed, Date.Thu, Date.Fri, Date.Sat, Date.Sun ]
+
 
 viewTableHeaders : Html Msg
 viewTableHeaders =
     tr []
-        ( [ th [] [ text "" ] ]
-              ++ List.map (\d -> th [] [ text <| toString d ]) weekDays
+        ([ th [] [ text "" ] ]
+            ++ List.map (\d -> th [] [ text <| toString d ]) weekDays
         )
 
-viewWeeklyActions : List Action -> List ActionLog -> List ( Html Msg )
+
+viewWeeklyActions : List Action -> List ActionLog -> List (Html Msg)
 viewWeeklyActions actions logs =
     List.map
-        (\a -> tr []
-             ( [ td [] [ text a.name ] ]
-                  ++
-                   viewWeekDay a (List.filter (\l -> l.actionId == a.id) logs)
-             )
+        (\a ->
+            let
+                logsForAction =
+                    (List.filter (\l -> l.actionId == a.id) logs)
+            in
+                tr [ class <| classesForActionRow a logsForAction ] <|
+                    [ viewActionLabelCell a ]
+                        ++ viewWeekDay a logsForAction
         )
         actions
 
-viewWeekDay : Action -> List ActionLog -> List ( Html Msg )
+
+viewActionLabelCell : Action -> Html Msg
+viewActionLabelCell action =
+    td [ onClick <| CreateActionLog action ] [ text action.name ]
+
+
+classesForActionRow : Action -> List ActionLog -> String
+classesForActionRow action logs =
+    if List.length logs >= action.perWeek then
+        "action-week-complete"
+    else
+        "action-week-incomplete"
+
+
+viewWeekDay : Action -> List ActionLog -> List (Html Msg)
 viewWeekDay action logs =
     List.map
-        ( \d -> td [] ( viewLogsForDay d logs ) )
+        (\d -> td [ class <| classesForActionCell d action logs ] (viewLogsForDay d logs))
         weekDays
 
-viewLogsForDay : Day -> List ActionLog -> List ( Html Msg )
+
+classesForActionCell : Day -> Action -> List ActionLog -> String
+classesForActionCell d action logs =
+    if List.length (logsForDay d logs) >= action.perDay then
+        "action-day-complete"
+    else
+        "action-day-incomplete"
+
+
+viewLogsForDay : Day -> List ActionLog -> List (Html Msg)
 viewLogsForDay day logs =
     List.map (\_ -> viewStar) (logsForDay day logs)
 
+
 viewStar : Html Msg
-viewStar = span [ class "glyphicon glyphicon-star" ] []
+viewStar =
+    span [ class "glyphicon glyphicon-star" ] []
+
 
 logsForDay : Day -> List ActionLog -> List ActionLog
 logsForDay day =
     List.filter
-        ( \log ->
-              case Date.fromString log.createdAt of
-                  Ok date -> Date.dayOfWeek date == day
-                  Err _ -> False
+        (\log ->
+            case Date.fromString log.createdAt of
+                Ok date ->
+                    Date.dayOfWeek date == day
+
+                Err _ ->
+                    False
         )
